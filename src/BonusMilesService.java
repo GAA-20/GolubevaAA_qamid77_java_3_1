549500cef6d1d5a7b72c5793c723a60cede769e5
Alexandra <sana201193@gmail.com>
2024-04-03 21:16:13 +0700

    Reformat code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,10 +1,9 @@
 public class BonusMilesService {
-   int calculate(int ticketPrise)
-   {
-       int bonusParam = 20; //количество рублей для одной бонусной мили
+    int calculate(int ticketPrise) {
+        int bonusParam = 20; //количество рублей для одной бонусной мили
 
-       int miles = ticketPrise / bonusParam;
+        int miles = ticketPrise / bonusParam;
 
-       return miles;
-   }
+        return miles;
+    }
 }
